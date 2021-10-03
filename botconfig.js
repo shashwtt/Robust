5a42937b6878a9d54b26584c596e82c4b3df21fe
cy-bug <69616790+cy-bug@users.noreply.github.com>
2021-10-03 23:59:38 +0530

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -16,7 +16,7 @@ module.exports = {
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
   Permissions: 2205280576, //Bot Inviting Permissions
-  Website: process.env.Website || "https://Discord-MusicBot.shashwatdubey.repl.co", //Website where it was hosted at includes http or https || Use "0.0.0.0" if you using Heroku
+  Website: process.env.Website || "0.0.0.0", //Website where it was hosted at includes http or https || Use "0.0.0.0" if you using Heroku
 
   //Lavalink
    Lavalink: {
